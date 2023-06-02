f1c4ad4709a0c2270605b94d269cb5bc3e03c9cc
SergioTGarcia <garciatormente@gmail.com>
2023-06-02 20:08:45 -0300

    app sorteio de nomes

---
+++ acess/js/script.js
@@ -0,0 +1,40 @@
+const btnSortear = document.querySelector(".btn-prize-draw")
+const textarea = document.querySelector("textarea")
+const tagResult = document.querySelector(".results p")
+const popUpResults = document.querySelector(".results")
+const btnToclose = document.querySelector(".to-close")
+//PEGANDO DADOS DO FORMULÁRIO
+function formData() {
+  //RECUPERANDO DADOS DO CAMPO (TEXTAREA)
+  const valorCampo = textarea.value
+  const listaNomes = valorCampo.split(",")
+  const tamanhoArray = listaNomes.length
+  const posicaoNome = gerarNumeroAleatorio(tamanhoArray)
+  const nome = listaNomes[posicaoNome]
+  mostrarResultado(nome)
+
+  textarea.value = ""
+}
+//ADICIONANDO EVENTO DE CLICK NO BOTÃO E EXECUTANDO A FUNÇÃO
+btnSortear.addEventListener("click", formData)
+//GERANDO NÚMERO ALEATÓRIO
+function gerarNumeroAleatorio(tamanhoArray) {
+  //RETORNA O NÚMERO
+  return Math.floor(Math.random() * (tamanhoArray - 0) + 0)
+
+}
+
+function mostrarResultado(nome) {
+
+  tagResult.innerText = `O nome sorteado foi: ${nome}`
+  popUpResults.classList.add("showresult")
+
+}
+
+
+function fecharPopUp() {
+
+  popUpResults.classList.remove("showresult")
+
+}
+btnToclose.addEventListener("click", fecharPopUp)
